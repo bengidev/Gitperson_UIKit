c975937ab3b4943fc4148dea60b1a4dff57d6035
B e n g i <bambang.trd17@gmail.com>
2024-01-04 12:14:10 +0700

    refactor: initialize OnboardingView to use in view

--- Gitperson/Gitperson/Features/Onboarding/Views/OnboardingViewController.swift
+++ Gitperson/Gitperson/Features/Onboarding/Views/OnboardingViewController.swift
@@ -8,9 +8,32 @@
 import UIKit
 
 final class OnboardingViewController: UIViewController {
+    private let onboardingView = OnboardingView()
 
+
+    // MARK: Initializers
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @available(*, unavailable)
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        fatalError("awakeFromNib() has not been implemented")
+    }
+    
     override func loadView() {
         super.loadView()
+        
+        setupViews()
     }
     
     override func viewDidLoad() {
@@ -18,4 +41,9 @@ final class OnboardingViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    private func setupViews() -> Void {
+        view = onboardingView
+    }
+
 }
