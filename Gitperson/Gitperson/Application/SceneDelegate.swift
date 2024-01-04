a34b0d1199721bf9d18c52ecc729753fd7764c0b
B e n g i <bambang.trd17@gmail.com>
2024-01-04 12:12:46 +0700

    fix: change rootView into OnboardingViewController

--- Gitperson/Gitperson/Application/SceneDelegate.swift
+++ Gitperson/Gitperson/Application/SceneDelegate.swift
@@ -17,11 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
         
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = navigationController
+        window?.rootViewController = OnboardingViewController()
         window?.makeKeyAndVisible()
     }
     
