27a98c533a9564c109fcb377a97c871cbc03a6d5
B e n g i <bambang.trd17@gmail.com>
2024-01-04 11:58:20 +0700

    feat: add SnapKit from CocoaPods

---
+++ Gitperson/Gitperson/Features/Onboarding/Views/OnboardingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingViewController.swift
+//  Gitperson
+//
+//  Created by Bambang Tri Rahmat Doni on 04/01/24.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
