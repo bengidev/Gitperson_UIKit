465a73229b95c5165f4c9baa979676e8fb30ce02
B e n g i <bambang.trd17@gmail.com>
2024-01-04 11:03:05 +0700

    refactor: clean structures without storyboards

---
+++ Gitperson/Gitperson/Features/Home/Views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gitperson
+//
+//  Created by Bambang Tri Rahmat Doni on 04/01/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .systemRed
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
